527d6fc442bce4b43f6bd58e663cb8fcbaecd9e8
IsmielAbir <ismielabir286@gmail.com>
2022-11-23 19:24:17 +0600

    Updated

--- Trip.py
+++ Trip.py
@@ -1,10 +1,10 @@
 class Trip:
-    def __init__(self, trip_cities, start_date) -> None:
+    def __init__(self, trip_cities, aircraft, price, start_date, route= '') -> None:
         self.trip_cities = trip_cities
         self.start_date = start_date
-        self.aircraft = None
-        self.trip_route = None
-        self.cost = None
+        self.aircraft = aircraft
+        self.trip_route = route
+        self.price = price
         
         
     def __repr__(self) -> str:
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from TravelAgent import TravelAgent
 def main():
     travel_agent = TravelAgent('Blue Sky')
     trip_info1 = travel_agent.set_trip_one_city_one_way('DAC', 'PRA', '05/04/2026')
-    print('aircraft', trip_info1[0])
-    print('price', trip_info1[1])
+    print('aircraft', trip_info1.aircraft)
+    print('price', trip_info1.cost)
     
 
 if __name__ == '__main__':
